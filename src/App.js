61f43ea2314f4ca116280b839686fccc35504067
Natallia <natalliakrup@gmail.com>
2022-05-18 12:11:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { dataHotel } from './dataHote';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const[hotel, setHotel] = useState(dataHotel);
+	const removeHotel =(id)=>{
+		const newHotel = hotel.filter(itemNewHotel => itemNewHotel.id !== id)
+		setHotel(newHotel)
+	}
+
+	const [showText, setShowText] = useState(false)
+	const btnMoreText=(item)=>{
+		item.showMore = !item.showMore
+		setShowText (!showText)
+	}
+
+	const reloadPage =()=>{
+		window.location.reload()
+	}
+
+	return (
+		<div className='thema_block'>
+		<div  className='container'>
+			<h1>New York City top {hotel.length} Hotels</h1>
+
+			{hotel.map((item =>{
+				const { id, hotelName, description, image, source, showMore } = item;
+				return(
+				<div className='container_flex' key={id}>
+					
+						<h2>{id} - {hotelName}</h2>
+						<p>{showMore? description + "  ": description.substring(0,230) + " ... "}
+						<button onClick={()=> btnMoreText(item)} className='btn_more'>{showMore? "show less": " show more"}</button>
+						</p>
+						<img src={image} alt="Hotel"/>
+						<p>Source: <a href={source} target="_blank" rel="noreferrer">Booking.com</a></p>
+					
+						<button className='btn_remove' onClick={()=> removeHotel(id)}>Remove</button>
+					
+				</div>
+				)
+			}))}
+			<div  className='btn_reload'>
+				<button className='btn_remove' onClick={()=> reloadPage()}>Reload page</button>
+			</div>
+		</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/dataHote.js
@@ -0,0 +1,28 @@
+export const dataHotel =[
+	{
+		id:1,
+		hotelName:"Hotel Beacon",
+		description:"This hotel has rooms and suites with kitchenettes and living room seating areas. The Hotel Beacon is on Manhattan’s Upper West Side, 805 m from Central Park. Each room and suite offers free cable on flat-screen TVs, iPod docks and WiFi access. The kitchenettes have a microwave, refrigerator and coffeemaker. Suites at the hotel have a living room with a sofa bed. The quiet and comfortable lobby offers space for visiting or reading and relaxing. There’s a business center for guest use and the hotel offers laundry service and luggage storage. The Hotel Beacon is 322 m from the American Museum of Natural History, and Lincoln Center Plaza is 1.1 km away.",
+		image: "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/39038995.jpg?k=f7554d11e8bba0b84366408dcac342cae0251daee63689b1c06614d1d81cfc86&o=&hp=1",
+		source: "https://www.booking.com/hotel/us/beacon.ru.html?aid=376376&label=bookings-name-x9XjJRNqhtTGZ30YXWfPUQS410902425082%3Apl%3Ata%3Ap1%3Ap22.563.000%3Aac%3Aap%3Aneg%3Afi%3Atikwd-65526620%3Alp9041931%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9Yf23yREhrOV9YczHwt1OUN4&sid=00bdc3f4a2c9c44a9846a353ab6dec3e&checkin=2022-05-18;checkout=2022-05-29;dest_id=56927;dest_type=hotel;dist=0;group_adults=2;group_children=0;hapos=1;hpos=1;no_rooms=1;req_adults=2;req_children=0;room1=A%2CA;sb_price_type=total;soh=1;sr_order=popularity;srepoch=1652784802;srpvid=9b0f4c90fc5d0015;type=total;ucfs=1&#no_availability_msg",
+		snowMore:false
+	},
+
+	{
+		id:2,
+		hotelName:"Lotte New York Palace",
+		description:"All rooms at this Midtown-Manhattan hotel come with a marble bathroom and some rooms offer a view of the Cathedral. A minibar and a desk are also included in the rooms. The property boats a 7000-square-feet fitness centre and spa. The Villard is open for breakfast and weekend brunches. Lotte New York also has an onsite bakery, two lounges and in-room dining options.",
+		image:"https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/51410533.jpg?k=54c069a15e872c81b5dbd3f07066cf318247fb58c1c2d42e35f8dbe6f07479ff&o=&hp=1",
+		source: "https://www.booking.com/hotel/us/new-york-palace.ru.html?aid=376376&label=bookings-name-x9XjJRNqhtTGZ30YXWfPUQS410902425082%3Apl%3Ata%3Ap1%3Ap22.563.000%3Aac%3Aap%3Aneg%3Afi%3Atikwd-65526620%3Alp9041931%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9Yf23yREhrOV9YczHwt1OUN4&sid=00bdc3f4a2c9c44a9846a353ab6dec3e&checkin=2022-05-18;checkout=2022-05-29;dest_id=357419;dest_type=hotel;dist=0;group_adults=2;group_children=0;hapos=1;hpos=1;no_rooms=1;req_adults=2;req_children=0;room1=A%2CA;sb_price_type=total;soh=1;sr_order=popularity;srepoch=1652784962;srpvid=77b44ce0b76b01e1;type=total;ucfs=1&#no_availability_msg",
+		snowMore:false
+	},
+
+	{
+		id:3,
+		hotelName:"InterContinental New York Barclay Hotel, an IHG Hotel",
+		description:" New York Barclay InterContinental includes an iPod docking station and cable TV with a DVD player. A minibar and coffee facilities are also provided. The rooms are decorated in gold tones. InterContinental Barclay New York offers concierge services and a gym. Luxury shops and restaurants along Fifth Ave are just a 5-minute walk from the InterContinental New York Barclay. Central Park is a 20-minute walk from the hotel and Rockefeller Center is a 10-minute walk away.",
+		image: "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/261355520.jpg?k=92f96913d29ac4bb951d07641456f2668b2dc7bbd5a4ae007baa97f5f379757a&o=&hp=1",
+		source: "https://www.booking.com/hotel/us/icg-the-barclay-new-york.ru.html?aid=376376&label=bookings-name-x9XjJRNqhtTGZ30YXWfPUQS410902425082%3Apl%3Ata%3Ap1%3Ap22.563.000%3Aac%3Aap%3Aneg%3Afi%3Atikwd-65526620%3Alp9041931%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9Yf23yREhrOV9YczHwt1OUN4&sid=00bdc3f4a2c9c44a9846a353ab6dec3e&all_sr_blocks=5869545_246084040_2_2_0;checkin=2022-05-18;checkout=2022-05-29;dest_id=58695;dest_type=hotel;dist=0;group_adults=2;group_children=0;hapos=1;highlighted_blocks=5869545_246084040_2_2_0;hpos=1;matching_block_id=5869545_246084040_2_2_0;no_rooms=1;req_adults=2;req_children=0;room1=A%2CA;sb_price_type=total;sr_order=popularity;sr_pri_blocks=5869545_246084040_2_2_0__666045;srepoch=1652785001;srpvid=75304cf4092302ef;type=total;ucfs=1&#hotelTmpl",
+		snowMore:false
+	},
+]
